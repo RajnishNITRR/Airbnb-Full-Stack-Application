3b6974de55ea7e042e41922bc777638c67c067f9
Rajnish Kumar <rajnish1800084@gmail.com>
2024-09-07 00:31:02 +0530

    load data

--- models/listing.js
+++ models/listing.js
@@ -2,19 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    description: String,
-    image: {
-        type: String,
-        default: "https://cdn.pixabay.com/photo/2021/08/27/01/33/bedroom-6577523_1280.jpg",
-        set: (v) => v === "" ? "https://cdn.pixabay.com/photo/2021/08/27/01/33/bedroom-6577523_1280.jpg" : v,
-    },
-    price: Number,
-    location: String,
-    country: String,
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    filename: { type: String },
+    url: { type: String }
+  },
+  price: Number,
+  location: String,
+  country: String,
 });
+
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
