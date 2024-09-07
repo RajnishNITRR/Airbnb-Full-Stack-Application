80ba0ef226737061b43a8a20933c7d0be4617616
Rajnish Kumar <rajnish1800084@gmail.com>
2024-09-07 20:40:37 +0530

    Read Route complete

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js")
+const ejs = require("ejs");
+const path = require("path");
 const port = 8080;
 
 const MONGO_URL = "mongodb://localhost:27017/wanderlust";
@@ -20,18 +22,47 @@ app = express();
 app.listen(port, () => {
     console.log(`server running at ${port}`);
 })
-app.get("/testListing", async (req, res) => {
-    let sampleListing = new Listing({
-        title: "my home ",
-        description: "By the beach",
-        price: 1200,
-        location: "calngute goa",
-        country: "India",
-    });
-    await sampleListing.save();
-    console.log("sample is saved");
-    res.send("successfull");
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.set(express.urlencoded({extended:true}));
+
+// Index Route
+app.get("/listings", async (req,res)=>{
+   const allListing=  await Listing.find({});
+   res.render("listings/index.ejs",{allListing})
 });
+
+
+app.get("/listings/:id", async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show",{listing});
+
+})
+
+
+
+
+
+
+
+
+
+
+
+// app.get("/testListing", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "my home ",
+//         description: "By the beach",
+//         price: 1200,
+//         location: "calngute goa",
+//         country: "India",
+//     });
+//     await sampleListing.save();
+//     console.log("sample is saved");
+//     res.send("successfull");
+// });
 app.get("/", (req, res) => {
     res.send("Hi i am ");
     console.log("get request successfull");
